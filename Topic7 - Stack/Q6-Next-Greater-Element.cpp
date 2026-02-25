94ae355c9a1608c74e401fe74ff6501cf773200b
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2026-02-25 17:17:38 +0530

    Day 37 : Stack Q-Next Greater Element

---
+++ Topic7 - Stack/Q6-Next-Greater-Element.cpp
@@ -0,0 +1,63 @@
+// * Question :- Next Greater Element
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+vector<int> nextGreaterElement(vector<int> nums)
+{
+  int n = nums.size();
+
+  // stack me greater elements ko store krenge
+  stack<int> st;
+  // stack me array ka last index ka element push kr diya hai
+  st.push(nums[n - 1]);
+
+  // res vector result array store krne ke liye
+  vector<int> res(n);
+  // result array ke last index pr -1 push kr diya hai
+  res[n - 1] = -1;
+
+  // traverse the array from last to first
+  for (int i = n - 2; i >= 0; i--)
+  {
+    // jab tak stack empty nhi ho jata
+    // tab tak stack ke top se vo elements pop kro jo nums[i] se chote/smaller hai
+    while (!st.empty() && st.top() <= nums[i])
+    {
+      st.pop();
+    }
+
+    // agar stack empty hai to result array me -1 store kro
+    if (st.empty())
+    {
+      res[i] = -1;
+    }
+
+    // iss ka matlab hai ke stack ke top me nums[i] se big/bada element hai uss ko result array me push kro
+    else
+    {
+      res[i] = st.top();
+    }
+
+    // current element ko stack me push kro pta nhi chalta kb kaam aa jaye
+    st.push(nums[i]);
+  }
+
+  return res;
+}
+int main()
+{
+  vector<int> nums = {4, 1, 2, 5, 3};
+
+  vector<int> result = nextGreaterElement(nums);
+
+  cout << "[ ";
+  for (int i = 0; i < result.size(); i++)
+  {
+    cout << result[i] << " ";
+  }
+  cout << "]";
+
+  return 0;
+}
